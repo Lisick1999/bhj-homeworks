f7a4801b57325c92724aaebc8397ed066ce0cf27
Fox Say <Lisick1999@yandex.ru>
2024-03-31 16:42:10 +0300

    first commit dropdown

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,21 @@
+const itemValue = document.getElementsByClassName('dropdown__value');
+const textLink = document.getElementsByClassName('dropdown__link');
+
+
+
+for(let i = 0; i < itemValue.length; i++) {
+    itemValue[i].onclick = function(){        
+       let list = this.closest(".dropdown");
+       list.querySelector(".dropdown__list").classList.add("dropdown__list_active");       
+    };
+}
+
+
+for(let i = 0; i < textLink.length; i++) { 
+    textLink[i].onclick = function(){        
+        let list = this.closest(".dropdown");
+        list.querySelector(".dropdown__list").classList.remove("dropdown__list_active");
+        list.querySelector(".dropdown__value").textContent = this.textContent;
+
+        return false;
+}; }
