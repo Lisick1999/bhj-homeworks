686a8024d7f12c5f56bf36afa98b1e7da8266513
Fox Say <Lisick1999@yandex.ru>
2024-04-16 17:59:25 +0300

    the click handler has been changed

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,18 +1,28 @@
-const hasTooltip = Array.from(document.querySelectorAll(".has-tooltip"))
-const tooltip = document.createElement("div")
-tooltip.classList.add("tooltip")
+const hasTooltip = Array.from(document.querySelectorAll(".has-tooltip"));
+const tooltip = document.createElement("div");
+tooltip.classList.add("tooltip");
 
 for (let item of hasTooltip) {
+
     item.addEventListener("click", (event) => {
-        let position = item.getBoundingClientRect()
+        let position = item.getBoundingClientRect();
 
         event.preventDefault()
-        item.insertAdjacentElement("afterend", tooltip)
-        let attr = item.getAttribute("title")
-        tooltip.textContent = attr
-        tooltip.classList.toggle("tooltip_active")
 
-        tooltip.style.top = position.bottom + 'px';
-        tooltip.style.left = position.left + 'px';
+        if(tooltip.innerHTML != item.title) {
+            tooltip.innerHTML = item.title;
+            tooltip.classList.add('tooltip_active');
+
+            item.insertAdjacentElement("afterend", tooltip);
+
+            let attr = item.getAttribute("title");
+            tooltip.textContent = attr;
+            
+            tooltip.style.top = position.bottom + 'px';
+            tooltip.style.left = position.left + 'px';
+
+        } else {
+            tooltip.classList.toggle('tooltip_active');
+        }
     })
 }
